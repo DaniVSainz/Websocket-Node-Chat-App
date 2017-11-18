819a2615d02c4f6a60417c89eca923fb874afa99
DaniVSainz <danivsainz@gmail.com>
2017-11-17 22:01:17 -0500

    socket .io shenanigans, currently added disconected event watcher

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const socketIO = require('socket.io');
 
 const path = require('path');
@@ -6,11 +7,16 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected')
+});
 
-app.listen(port, function() {
+server.listen(port, function() {
     console.log(`Server is up on ${port}`);
 });
 
