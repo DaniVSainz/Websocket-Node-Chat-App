c8e92acd330cd2b0cb10b30daa11b6d518ed6b13
DaniVSainz <danivsainz@gmail.com>
2017-11-17 21:11:16 -0500

    .

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socketIO = require('socket.io');
 
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
+
 
 app.listen(port, function() {
     console.log(`Server is up on ${port}`);
