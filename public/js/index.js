7f54cd4a279f19035ed162305d97918d4e348f01
DaniVSainz <danivsainz@gmail.com>
2017-12-21 14:27:41 -0500

    .

--- public/js/index.js
+++ public/js/index.js
@@ -16,11 +16,19 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  
-  
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
@@ -29,30 +37,18 @@ jQuery('#message-form').on('submit', function (e) {
   });
 });
 
-
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function(){
-  if(!navigator.geolocation){
-    return alert('Geolocation not supported by your browser');
-  } 
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
 
-  navigator.geolocation.getCurrentPosition(function(position){
+  navigator.geolocation.getCurrentPosition(function (position) {
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
-  }, function() {
-    alert('Unable to fetch location');
+  }, function () {
+    alert('Unable to fetch location.');
   });
-
-  socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-  
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
-  });
-
 });
