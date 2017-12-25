7318deeb819f10a61981539d9d6d5aae97f15504
DaniVSainz <danivsainz@gmail.com>
2017-12-25 01:33:21 -0500

    Add timestamps

--- playground/time.js
+++ playground/time.js
@@ -1,9 +1,21 @@
 var moment = require('moment');
 
+// Jan 1st 1970 00:00:10 am
 
-var date = moment();
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
 
-console.log(date.format('h:mm a'));
+// var date = moment();
+// date.add(100, 'year').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
 
-var newDate = moment().valueOf();
-console.log(newDate);
+// 10:35 am
+// 6:01 am
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp)
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
