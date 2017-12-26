3ba2216983c208cf7ec4e2c791dd8717dc533c05
danivsainz <danivsainz@gmail.com>
2017-12-26 18:04:22 -0500

    location broadcasts are no longer global

--- public/js/chat.js
+++ public/js/chat.js
@@ -48,6 +48,12 @@ socket.on('newMessage', function (message) {
 
 socket.on('updateUserList', function(users) {
   console.log('Users list', users);
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function(user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -69,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
